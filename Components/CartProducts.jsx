9c8b53f41ac03f21cab7793c5820875c63546da7
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-03-31 16:11:14 +0600

    creat AllProducts.jsx and CartProducts.jsx

---
+++ Components/CartProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartProducts = () => {
+  return (
+    <div>
+      <h1>Hello Cart</h1>
+    </div>
+  );
+};
+
+export default CartProducts;
--- Components/MainSection.jsx
+++ Components/MainSection.jsx
@@ -1,9 +1,10 @@
 import React, { use, useState } from 'react';
-import CheckImg from '../src/assets/Vector.png';
+import AllProducts from './AllProducts';
+import CartProducts from './CartProducts';
+
 
 const MainSection = ({ data }) => {
-  const useData = use(data);
-  console.log(useData);
+  // console.log(useData);
 
   const [products, setProducts] = useState('Products');
 
@@ -21,12 +22,12 @@ const MainSection = ({ data }) => {
 
       <div className="w-44 mx-auto p-1 border border-gray-200 rounded-3xl mt-5 mb-7 text-white">
         <button
-          onClick={()=>setProducts("Products")}
+          onClick={() => setProducts('Products')}
           className={`px-2 py-1.5 mr-2 ${products === 'Products' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'} rounded-3xl hover:cursor-pointer`}>
           Products
         </button>
         <button
-          onClick={() => setProducts("Cart")}
+          onClick={() => setProducts('Cart')}
           className={`px-3.5 py-1.5 ${products === 'Cart' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'}  rounded-3xl hover:cursor-pointer`}>
           Cart (0)
         </button>
@@ -34,56 +35,9 @@ const MainSection = ({ data }) => {
 
       {/* Main part */}
       {products === 'Products' ? (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {useData.map((data) => (
-            <div className="border border-gray-200 p-5 rounded-3xl hover:transition-all hover:duration-300 hover:shadow-xl hover:ease-in-out hover:-translate-y-2 space-y-3.5">
-              <div>
-                <div className="text-right text-red-500 text-md">
-                  <p
-                    className={`px-2.5 py-2 rounded-3xl inline-block ${
-                      data.tag === 'Popular'
-                        ? 'bg-[#e0e7ff] text-blue-500'
-                        : data.tag === 'New'
-                          ? 'bg-[#dcfce8] text-green-500'
-                          : data.tag === 'Best seller'
-                            ? 'bg-[#fff4c7] text-red-500'
-                            : 'bg-gray-200'
-                    }`}>
-                    {data.tag}
-                  </p>
-                </div>
-                <div className="p-3 border border-gray-300 inline-block rounded-full bg-[#f5f5f5]">
-                  <img className="h-8 w-8" src={data.icon} alt="" />
-                </div>
-              </div>
-              <h1 className="text-2xl font-semibold">{data.name}</h1>
-              <p className="text-gray-500">{data.description}</p>
-              <p>
-                <span className="text-3xl font-bold">${data.price}</span>/
-                {data.period}
-              </p>
-              <div>
-                {data.features.map((list) => (
-                  <li className="flex items-center gap-1">
-                    <img
-                      className="h-[15px] w-[15px]"
-                      src={CheckImg}
-                      alt="Check Image"
-                    />
-                    <p className="text-gray-500">{list}</p>
-                  </li>
-                ))}
-              </div>
-              <button class="btn btn-primary w-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-3xl transition-transform hover:-translate-y-2 duration-300 ease-in-out mt-6">
-                <span className="text-white">Buy Now</span>
-              </button>
-            </div>
-          ))}
-        </div>
+        <AllProducts data={data} />
       ) : (
-        <div>
-          <h1>Hello Cart</h1>
-        </div>
+        <CartProducts />
       )}
     </div>
   );
