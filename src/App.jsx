961abea5c7ca6edc0659b44f87a8a863a86bfe64
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-03-31 13:49:26 +0600

    fetch Digital Tools Fake data and creat card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import RatingSection from '../Components/RatingSection'
 import StartedSection from '../Components/StartedSection'
 import TransparentPricing from '../Components/TransparentPricing'
 import Footer from '../Components/Footer'
+import MainSection from '../Components/MainSection'
+
+
+const loadData = async () => {
+  const res = await fetch("/public/data.json");
+  return res.json();
+}
+
+const data = loadData();
+
 
 function App() {
 
@@ -13,6 +23,7 @@ function App() {
       <NavBar />
       <HeroSection />
       <RatingSection />
+      <MainSection data={data} />
       <StartedSection />
       <TransparentPricing />
       <Footer/>
