8d9eb062b8dc519b3379200bce4560a8995b8eee
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-04-01 15:37:40 +0600

    update products and cart button div

--- Components/MainSection.jsx
+++ Components/MainSection.jsx
@@ -14,7 +14,9 @@ const MainSection = ({ data, selectedProducts, setSelectedProducts }) => {
     <div className="my-34 max-w-11/12 md:max-w-10/12 lg:max-w-9/12 mx-auto">
       {/* Header part */}
       <div className="max-w-11/12 md:max-w-10/12 lg:max-w-9/12 mx-auto text-center">
-        <h1 className="text-4xl md:text-5xl font-bold">Premium Digital Tools</h1>
+        <h1 className="text-4xl md:text-5xl font-bold">
+          Premium Digital Tools
+        </h1>
         <p className=" md:text-xl font-semibold text-gray-500 my-7">
           Choose from our curated collection of premium digital products
           designed <br className="hidden md:block" /> to boost your productivity
@@ -22,19 +24,21 @@ const MainSection = ({ data, selectedProducts, setSelectedProducts }) => {
         </p>
       </div>
 
-      <div className="w-44 mx-auto p-1 border border-gray-200 rounded-3xl mt-5 mb-7 text-white">
-        <button
-          type='button'
-          onClick={() => setProducts('Products')}
-          className={`px-2 py-1.5 mr-2 ${products === 'Products' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'} rounded-3xl hover:cursor-pointer`}>
-          Products
-        </button>
-        <button
-          type='button'
-          onClick={() => setProducts('Cart')}
-          className={`px-3.5 py-1.5 ${products === 'Cart' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'}  rounded-3xl hover:cursor-pointer`}>
-          Cart ({cartLength})
-        </button>
+      <div className="text-center flex justify-center mx-auto mt-5 mb-7 text-white">
+        <div className="border border-gray-200 p-1 rounded-3xl">
+          <button
+            type="button"
+            onClick={() => setProducts('Products')}
+            className={`px-2 py-1.5 mr-2 ${products === 'Products' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'} rounded-3xl hover:cursor-pointer`}>
+            Products
+          </button>
+          <button
+            type="button"
+            onClick={() => setProducts('Cart')}
+            className={`px-3.5 py-1.5 ${products === 'Cart' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-white text-black'}  rounded-3xl hover:cursor-pointer`}>
+            Cart ({cartLength})
+          </button>
+        </div>
       </div>
 
       {/* Main part */}
