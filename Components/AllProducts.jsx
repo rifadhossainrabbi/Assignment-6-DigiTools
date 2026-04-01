5969f6e4dbbd852a83a663a5d2632c55790052ed
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-04-01 20:11:34 +0600

    fix responsive

--- Components/AllProducts.jsx
+++ Components/AllProducts.jsx
@@ -9,7 +9,7 @@ const AllProducts = ({
   // console.log(useData);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {data.map((data) => {
         return (
           <Card
--- Components/RatingSection.jsx
+++ Components/RatingSection.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 const RatingSection = () => {
   return (
     <div className="w-full mt-[50px] min-h-[20vh] bg-gradient-to-r from-[#4F39F6] to-[#9514FA]">
-      <div className="max-w-11/12 md:max-w-10/12 lg:max-w-9/12 mx-auto flex flex-col md:flex-row md:flex-wrap justify-center items-center py-14 text-white gap-10 md:gap-32">
+      <div className="max-w-11/12 md:max-w-10/12 lg:max-w-9/12 mx-auto flex flex-col md:flex-row justify-center items-center py-8 md:py-14 text-white gap-10 md:gap-32">
         <div className="text-center md:text-left">
           <h1 className="text-3xl md:text-5xl font-bold">50K+</h1>
           <p className="mt-2 md:mt-3.5 text-gray-200">Active Users</p>
         </div>
 
-        <div className="hidden md:block md:h-[50px] md:w-[1px] md:bg-gray-300 md:mx-6"></div>
+        <div className="hidden md:block md:h-14 md:w-[1px] md:bg-gray-300 md:mx-6"></div>
 
         <div className="text-center md:text-left">
           <h1 className="text-3xl md:text font-bold">200+</h1>
           <p className="mt-2 md:mt-3.5 text-gray-200">Premium Tools</p>
         </div>
 
-        <div className="hidden md:block md:h-[50px] md:w-[1px] md:bg-gray-300 md:mx-6"></div>
+        <div className="hidden md:block md:h-14 md:w-[1px] md:bg-gray-300 md:mx-6"></div>
 
         <div className="text-center md:text-left">
           <h1 className="text-3xl md:text font-bold">4.9</h1>
